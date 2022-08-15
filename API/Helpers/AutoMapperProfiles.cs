4e0623c337367c27f02ef00ebf87a2b74e8c005a
Ilia Iliev <ilia.iliev@accedia.com>
2022-08-15 14:18:23 +0300

    End of section 10

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -16,6 +16,8 @@ namespace API.Helpers
                     source.DateOfBirth.CalculateAge()));
                     
             CreateMap<Photo, PhotoDto>();
+
+            CreateMap<MemberUpdateDto, AppUser>();
         }
     }
 }

165167355890b0c7cebc4547d36f55f2c780fd49
Ilia Iliev <ilia.iliev@accedia.com>
2022-08-16 10:05:38 +0300

    End of section 11

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -25,15 +25,25 @@ export class MembersService {
   }
 
   getMember(username: string) {
-    const member = this.members.find(member => member.username === username);
-    if(member !== undefined) return of(member);
+    const member = this.members.find((member) => member.username === username);
+    if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member).pipe(map(() => {
-      const index = this.members.indexOf(member);
-      this.members[index] = member;
-    }));
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    );
+  }
+
+  setMainPhoto(photoId: Number) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photoId, {});
+  }
+
+  deletePhoto(photoId: Number) {
+    return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
   }
 }
